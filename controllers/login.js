1d7790cae03320e45f99fabdfefcf22b1140e4f5
Pouyan <pouyannc@gmail.com>
2023-09-13 17:29:15 -0400

    Await user query for login

--- controllers/login.js
+++ controllers/login.js
@@ -6,13 +6,12 @@ const User = require('../models/users');
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body;
 
-  const user = User.findOne({ username });
-  const passwordCorrect = user
+  const user = await User.findOne({ username });
+  const passwordCorrect = !user
     ? false
     : await bcrypt.compare(password, user.passwordHash);
 
   if (!(passwordCorrect)) return response.status(401).json({ error: 'invalid username or password' });
-
   const userForToken = {
     username: user.username,
     id: user._id,
